636599c3601cc0288d61ffb517ecd502892a07a5
KitaeJeong <wjdrlxo2013@gmail.com>
2025-07-14 10:36:01 +0900

    fixed some repository structure

--- chapter01/cal_time.c
+++ chapter01/cal_time.c
@@ -5,11 +5,11 @@
 int main(void) {
 	clock_t start, stop;
 	double duration;
-	start = clock();
+	start = clock(); // 측정 시작
 
 	for (int i = 0; i < 1000000; i++) {}
 
-	stop = clock();
+	stop = clock(); // 측정 종료
 	duration = (double)(stop - start) / CLOCKS_PER_SEC;
 	printf("실행 시간은 %f초 입니다.", duration);
 	return 0;
